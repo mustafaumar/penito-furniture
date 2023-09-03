fcb36a5663d39cea110afd762050f903734a81e6
mustafaumar2002@gmail.com <mustafaumar2002@gmail.com>
2023-09-03 14:24:22 +0100

    fix: Hero button styles

--- src/components/layout/Header/HeroSection.jsx
+++ src/components/layout/Header/HeroSection.jsx
@@ -20,7 +20,7 @@ const HeroSection = () => {
 					<button className="flex items-center justify-center bg-clr4 px-2 py-6 h-10 rounded-lg text-white">
 						Shop Here <FaArrowCircleRight className="text-clr3 ml-1" />
 					</button>
-					<button className="flex items-center justify-center px-2 py-6 h-10 rounded-lg text-black font-semibold shadow-sm hover:shadow-black">
+					<button className="flex items-center justify-center px-2 py-6 h-10 rounded-lg text-black font-semibold shadow-sm shadow-black">
 						<FaPlayCircle className="text-clr1 mr-1 text-2xl" /> Watch Here
 					</button>
 				</div>
